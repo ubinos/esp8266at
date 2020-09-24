72f7840a546b458badf17fd8f670154a264cf90a
Sungho Park <siminsungho@gmail.com>
2020-09-25 02:14:30 +0900

    change uart port to usart2

--- app/esp8266at_tester/arch/arm/cortexm/nucleof207zg/main.h
+++ app/esp8266at_tester/arch/arm/cortexm/nucleof207zg/main.h
@@ -31,30 +31,30 @@
 /* User can use this section to tailor USARTx/UARTx instance used and associated
    resources */
 /* Definition for USARTx clock resources */
-#define USARTx                           UART4
-#define USARTx_CLK_ENABLE()              __HAL_RCC_UART4_CLK_ENABLE();
-#define USARTx_RX_GPIO_CLK_ENABLE()      __HAL_RCC_GPIOC_CLK_ENABLE()
-#define USARTx_TX_GPIO_CLK_ENABLE()      __HAL_RCC_GPIOC_CLK_ENABLE()
+#define USARTx                           USART2
+#define USARTx_CLK_ENABLE()              __HAL_RCC_USART2_CLK_ENABLE();
+#define USARTx_RX_GPIO_CLK_ENABLE()      __HAL_RCC_GPIOD_CLK_ENABLE()
+#define USARTx_TX_GPIO_CLK_ENABLE()      __HAL_RCC_GPIOD_CLK_ENABLE()
 
-#define USARTx_FORCE_RESET()             __HAL_RCC_UART4_FORCE_RESET()
-#define USARTx_RELEASE_RESET()           __HAL_RCC_UART4_RELEASE_RESET()
+#define USARTx_FORCE_RESET()             __HAL_RCC_USART2_FORCE_RESET()
+#define USARTx_RELEASE_RESET()           __HAL_RCC_USART2_RELEASE_RESET()
 
 /* Definition for USARTx Pins */
-#define USARTx_TX_PIN                    GPIO_PIN_10
-#define USARTx_TX_GPIO_PORT              GPIOC
-#define USARTx_TX_AF                     GPIO_AF8_UART4
-#define USARTx_RX_PIN                    GPIO_PIN_11
-#define USARTx_RX_GPIO_PORT              GPIOC
-#define USARTx_RX_AF                     GPIO_AF8_UART4
+#define USARTx_TX_PIN                    GPIO_PIN_5
+#define USARTx_TX_GPIO_PORT              GPIOD
+#define USARTx_TX_AF                     GPIO_AF7_USART2
+#define USARTx_RX_PIN                    GPIO_PIN_6
+#define USARTx_RX_GPIO_PORT              GPIOD
+#define USARTx_RX_AF                     GPIO_AF7_USART2
 
 /* Definition for USARTx's NVIC IRQ and IRQ Handlers */
-#define USARTx_IRQn                      UART4_IRQn
-#define USARTx_IRQHandler                UART4_IRQHandler
+#define USARTx_IRQn                      USART2_IRQn
+#define USARTx_IRQHandler                USART2_IRQHandler
 
 /* WiFi module Reset pin definitions */
-#define ESP8266_RST_GPIO_PORT            GPIOC
-#define ESP8266_RST_PIN                  GPIO_PIN_12
-#define ESP8266_RST_GPIO_CLK_ENABLE()    __HAL_RCC_GPIOC_CLK_ENABLE()
+#define ESP8266_RST_GPIO_PORT            GPIOD
+#define ESP8266_RST_PIN                  GPIO_PIN_7
+#define ESP8266_RST_GPIO_CLK_ENABLE()    __HAL_RCC_GPIOD_CLK_ENABLE()
 
 /* Exported macro ------------------------------------------------------------*/
 /* Exported functions ------------------------------------------------------- */
