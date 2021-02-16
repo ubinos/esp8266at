d947f1dbb18562b6fb87b4a698eac625476919eb
Sungho Park <siminsungho@gmail.com>
2021-02-16 17:21:14 +0900

    add application, "esp8266at_tester_nucleof207zg_nostlink"

---
+++ app/esp8266at_tester_nucleof207zg_nostlink.cmake
@@ -0,0 +1,4 @@
+set_cache(UBINOS__BSP__STM32_RCC_HSE_CONFIG "ON" STRING)
+
+include(${CMAKE_CURRENT_LIST_DIR}/esp8266at_tester_nucleof207zg.cmake)
+
--- make/batch.mk
+++ make/batch.mk
@@ -26,6 +26,13 @@
 	@echo ""
 	@echo ""
 
+	@echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+	@echo ""
+	@echo ""
+	make -f makefile.mk $@ CONFIG_DIR=../app CONFIG_NAME=esp8266at_tester_nucleof207zg_nostlink
+	@echo ""
+	@echo ""
+
 
 	@echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
 	@echo ""
