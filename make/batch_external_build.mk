d9b6f6ce1ce701003b96d3d516ba575d88dcbb83
Sungho Park <siminsungho@gmail.com>
2021-03-19 13:23:11 +0900

    refactoring

--- make/batch_external_build.mk
+++ make/batch_external_build.mk
@@ -14,7 +14,7 @@ config configd clean cleand:
 	@echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
 	@echo ""
 	@echo ""
-	make -f makefile.mk $@ CONFIG_DIR=../app CONFIG_NAME=esp8266at_tester_nucleof207zg_rexternal_build
+	make -f makefile.mk $@ CONFIG_DIR=../app CONFIG_NAME=esp8266at_tester_nucleof207zg_external_build
 	@echo ""
 	@echo ""
 
