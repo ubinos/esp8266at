6e4e2dbfe7c00d32fcf495246082bc7b407e179b
Sungho Park <siminsungho@gmail.com>
2021-10-11 00:27:13 +0900

    refactoring

--- make/batch.mk
+++ make/batch.mk
@@ -22,13 +22,21 @@
 	@echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
 	@echo ""
 	@echo ""
-	make -f makefile.mk $@ CONFIG_DIR=../app CONFIG_NAME=esp8266at_tester_nucleof207zg
+	make -f makefile.mk $@ CONFIG_DIR=../app CONFIG_NAME=esp8266at_tester_nrf52840dk
+	@echo ""
+	@echo ""
+
+
+	@echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+	@echo ""
+	@echo ""
+	make -f makefile.mk $@ CONFIG_DIR=../app CONFIG_NAME=esp8266at_tester_nucleof207zg_nostlink
 	@echo ""
 	@echo ""
 	@echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
 	@echo ""
 	@echo ""
-	make -f makefile.mk $@ CONFIG_DIR=../app CONFIG_NAME=esp8266at_tester_nucleof207zg_nostlink
+	make -f makefile.mk $@ CONFIG_DIR=../app CONFIG_NAME=esp8266at_tester_nucleof207zg
 	@echo ""
 	@echo ""
 
