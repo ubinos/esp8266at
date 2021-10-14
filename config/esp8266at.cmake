9356e194a6220ee4ef099101afcff2dd0255f91c
Sungho Park <siminsungho@gmail.com>
2021-10-15 01:27:32 +0900

    update ubinos build scripts

--- config/esp8266at.cmake
+++ config/esp8266at.cmake
@@ -5,6 +5,7 @@
 #
 
 set(INCLUDE__ESP8266AT TRUE)
+set(PROJECT_UBINOS_LIBRARIES ${PROJECT_UBINOS_LIBRARIES} esp8266at)
 
 set_cache_default(ESP8266AT__USE_RESET_PIN FALSE BOOL "Use reset pin")
 set_cache_default(ESP8266AT__USE_CHIPSELECT_PIN TRUE BOOL "Use chip select pin")
