995ec8a2702bb42ded47d8340ffaa82187dd5d48
Sungho Park <siminsungho@gmail.com>
2022-07-04 20:55:06 +0900

    add ubinos config, esp8266at_tester_sphub - 02.

--- app/esp8266at_tester/arch/arm/cortexm/sphub/main.h
+++ app/esp8266at_tester/arch/arm/cortexm/sphub/main.h
@@ -12,13 +12,13 @@ extern esp8266at_t _g_esp8266at;
 
 /* Definition for ESP8266_UART (mikroBUS 1) */
 
-#define ESP8266_UART_TX_Pin ARDUINO_1_PIN
-#define ESP8266_UART_RX_Pin ARDUINO_0_PIN
+#define ESP8266_UART_TX_Pin NRF_GPIO_PIN_MAP(0,17)
+#define ESP8266_UART_RX_Pin NRF_GPIO_PIN_MAP(0,16)
 #if (ESP8266AT__USE_RESET_PIN == 1)
-    #define ESP8266_NRST_Pin ARDUINO_A3_PIN
+    #define ESP8266_NRST_Pin NRF_GPIO_PIN_MAP(1,0)
 #endif /* (ESP8266AT__USE_RESET_PIN == 1) */
 #if (ESP8266AT__USE_CHIPSELECT_PIN == 1)
-    #define ESP8266_CS_Pin ARDUINO_10_PIN
+    #define ESP8266_CS_Pin NRF_GPIO_PIN_MAP(1,1)
 #endif /* (ESP8266AT__USE_CHIPSELECT_PIN == 1) */
 
 #endif /* __MAIN_H */
